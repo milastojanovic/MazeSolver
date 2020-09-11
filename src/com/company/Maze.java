ef0b39ab70a227b9c5eca20d8f5e0d81d6c3a0c2
Mila Stojanovic <70701748+milastojanovic@users.noreply.github.com>
2020-09-11 13:39:34 +0200

    Refactoring Add Maze.java Update MazeSolver.java (a list of mazes can be solved)

---
+++ src/com/company/Maze.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.LinkedList;
+
+public class Maze {
+    public int[][] maze;
+    public LinkedList<Position> path = new LinkedList<>();
+    public Position start;
+}
