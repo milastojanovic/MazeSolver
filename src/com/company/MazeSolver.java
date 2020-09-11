ef208a84bf672af1c502382eeb26e366a3a96aff
Mila Stojanovic <70701748+milastojanovic@users.noreply.github.com>
2020-09-11 12:00:05 +0200

    MazeSolver complete

--- src/com/company/MazeSolver.java
+++ src/com/company/MazeSolver.java
@@ -5,11 +5,29 @@ import java.util.LinkedList;
 
 public class MazeSolver {
 
+//    static int[][] maze = {
+//            {2, 0, 1, 1},
+//            {1, 1, 1, 0},
+//            {0, 0, 0, 1}
+//    }; // Mila: start - second 1
+//    static int[][] maze = {
+//            {2, 0, 0, 1},
+//            {1, 1, 1, 0},
+//            {0, 0, 0, 1}
+//    };
+//    static int[][] maze = {
+//            {2, 1, 1, 1},
+//            {0, 0, 1, 1},
+//            {0, 0, 0, 1}
+//    };
     static int[][] maze = {
-            {2, 0, 1, 1},
-            {1, 1, 1, 0},
-            {0, 0, 0, 1}
-    }; // Mila: start - second 1
+            {1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
+            {0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1},
+            {1, 1, 1, 2, 0, 1, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1}
+    };
     // 0 = wall
     // 1 = path
     // 2 = destination
@@ -17,7 +35,8 @@ public class MazeSolver {
     static LinkedList<Position> path = new LinkedList<>(); // Mila: will be used as stack
 
     public static void main(String[] args) {
-        Position p = new Position(0, 3); // Mila: x - column, y - row
+//        Position p = new Position(0, 3); // Mila: x - column, y - row
+        Position p = new Position(4, 8); // Mila: x - column, y - row
 
         path.push(p); // Mila: start
 
@@ -28,49 +47,69 @@ public class MazeSolver {
             maze[y][x] = 0;
 
             // down (move down)
-            if (maze[y + 1][x] == 2) {
-                System.out.println("Moved down. You won!");
-                return;
-            } else if (maze[y + 1][x] == 1) {
-                System.out.println("Moved down");
-                path.push(new Position(y + 1, x));
-                continue; // Mila: go to the next iteration of the loop
+            if (isValid(y + 1, x)) {
+                if (maze[y + 1][x] == 2) {
+                    System.out.println("Moved down. You won!");
+                    return;
+                } else if (maze[y + 1][x] == 1) {
+                    System.out.println("Moved down");
+                    path.push(new Position(y + 1, x));
+                    continue; // Mila: go to the next iteration of the loop
+                }
             }
 
             // left (move left)
-            if (maze[y][x - 1] == 2) {
-                System.out.println("Moved left. You won!");
-                return;
-            } else if (maze[y][x - 1] == 1) {
-                System.out.println("Moved left");
-                path.push(new Position(y, x - 1));
-                continue; // Mila: go to the next iteration of the loop
+            if (isValid(y, x - 1)) {
+                if (maze[y][x - 1] == 2) {
+                    System.out.println("Moved left. You won!");
+                    return;
+                } else if (maze[y][x - 1] == 1) {
+                    System.out.println("Moved left");
+                    path.push(new Position(y, x - 1));
+                    continue; // Mila: go to the next iteration of the loop
+                }
             }
 
             // up (move up)
-            if (maze[y - 1][x] == 2) {
-                System.out.println("Moved up. You won!");
-                return;
-            } else if (maze[y - 1][x] == 1) {
-                System.out.println("Moved up");
-                path.push(new Position(y - 1, x));
-                continue; // Mila: go to the next iteration of the loop
+            if (isValid(y - 1, x)) {
+                if (maze[y - 1][x] == 2) {
+                    System.out.println("Moved up. You won!");
+                    return;
+                } else if (maze[y - 1][x] == 1) {
+                    System.out.println("Moved up");
+                    path.push(new Position(y - 1, x));
+                    continue; // Mila: go to the next iteration of the loop
+                }
             }
 
             // right (move right)
-            if (maze[y][x + 1] == 2) {
-                System.out.println("Moved right. You won!");
-                return;
-            } else if (maze[y][x + 1] == 1) {
-                System.out.println("Moved right");
-                path.push(new Position(y, x + 1));
-                continue; // Mila: go to the next iteration of the loop
+            if (isValid(y, x + 1)) {
+                if (maze[y][x + 1] == 2) {
+                    System.out.println("Moved right. You won!");
+                    return;
+                } else if (maze[y][x + 1] == 1) {
+                    System.out.println("Moved right");
+                    path.push(new Position(y, x + 1));
+                    continue; // Mila: go to the next iteration of the loop
+                }
             }
 
             path.pop();
+            System.out.println("Moved back");
             if (path.size() <= 0) {
                 System.out.println("No path");
+                return;
             }
         }
+    }
+
+    public static boolean isValid(int y, int x) {
+        if (y < 0 ||
+            y >= maze.length ||
+            x < 0 ||
+            x >= maze[y].length) {
+            return false;
+        }
+        return true;
     }
 }
